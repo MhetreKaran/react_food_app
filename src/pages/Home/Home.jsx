057231cace6606759a08ab139b1db28994fd781a
karan <karanmhetre9@gmail.com>
2024-06-22 17:37:40 +0530

    complete footer design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,19 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import Footer from '../../components/Footer/Footer';
 function Home() {
   const [category, setCategory] = useState('All');
   console.log(category);
   return (
+    <>
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
     </div>
+    <Footer />
+    </>
   )
 }
 
