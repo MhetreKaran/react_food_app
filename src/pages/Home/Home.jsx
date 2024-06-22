b8bf42bc981a9284f29dd0167509b2d26b3b1381
karan <karanmhetre9@gmail.com>
2024-06-22 17:07:36 +0530

    completed main container design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 function Home() {
+  const [category, setCategory] = useState('All');
+  console.log(category);
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
